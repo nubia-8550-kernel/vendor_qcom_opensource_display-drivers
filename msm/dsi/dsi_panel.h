615115204da9c6253dfa6f286109f84138503e63
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-13 09:33:09 +0300

    display-drivers: expose forcing FOD UI for debugging
    Change-Id: I1f1b5344c9f98b541dc5c99e5d6f9c5560d8340c

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -370,6 +370,7 @@ struct dsi_panel {
 	unsigned int fod_dim_lut_len;
 	u8 fod_dim_alpha;
 	bool fod_ui;
+	bool force_fod_ui;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -514,5 +515,6 @@ int zte_dsi_panel_tx_cmd_set(struct dsi_panel *panel, enum dsi_cmd_set_type type
 int dsi_panel_set_hbm_on(struct dsi_panel *panel);
 bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
 void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
+bool dsi_panel_get_force_fod_ui(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
