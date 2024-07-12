3bf151391f71cc1f52d9e215fcef872887310aa9
Abhay Singh Gill <abhaygill017@gmail.com>
2024-07-12 16:57:20 +0530

    display-drivers: Don't use fod hbm if the userspace backlight value is more
    Change-Id: I6569d2b3c20a4df40c727190059e3098582c57db
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- msm/sde/sde_connector.c
+++ msm/sde/sde_connector.c
@@ -1176,6 +1176,9 @@ static int _sde_connector_update_finger_hbm_status(
 		return 0;
 	}
 
+	if (display->panel->bl_config.real_bl_level >= display->panel->bl_config.bl_hbm_level)
+	        return 0;
+
 	SDE_ATRACE_BEGIN("_sde_connector_update_finger_hbm_statuss");
         if (!c_conn->fingerlayer_dirty)
                 finger_hbm_flag = status;
